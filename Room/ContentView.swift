a8d7f67cfb5e9340807c3a53e755880134148ba5
JJ <john@race.it>
2019-06-09 16:09:15 +0200

    Added the files needed from the demo WWDC 2019 204 lecture.

--- Room/ContentView.swift
+++ Room/ContentView.swift
@@ -9,15 +9,83 @@
 import SwiftUI
 
 struct ContentView : View {
+    @ObjectBinding var store = RoomStore()
+    //var rooms: [Room] = []
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            List {
+                Section {
+                    Button(action: addRoom) {
+                        Text("Add Room")
+                    }
+                }
+                
+                Section {
+                    ForEach(store.rooms) { room in
+                        RoomCell(room: room)
+                        }
+                        .onDelete(perform: delete)
+                        .onMove(perform: move)
+                }
+                }
+                .navigationBarTitle(Text("Rooms"))
+                .navigationBarItems(trailing: EditButton())
+                .listStyle(.grouped)
+        }
     }
+    
+    func addRoom() {
+        store.rooms.append(Room(name: "Hall 2", capacity: 2000))
+    }
+    
+    
+    func delete(at offsets: IndexSet) {
+        //store.rooms.remove(atOffsets : offsets)
+        offsets.sorted { $0 > $1 }.forEach { store.rooms.remove(at: $0) }
+    }
+    
+    func move(from source: IndexSet, to destination: Int) {
+        //stores.room.move(fromOffsets: source, toOffset: destination)
+        source.sorted { $0 > $1 }.forEach { store.rooms.insert(store.rooms.remove(at: $0), at: destination) }
+    }
+    
+    //func delete(at offsets: IndexSet) {
+    //    store.rooms.remove
+    //}
 }
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView(store: RoomStore(rooms: testData))
+            ContentView(store: RoomStore(rooms: testData))
+                .environment(\.sizeCategory, .extraExtraExtraLarge)
+            ContentView(store: RoomStore(rooms: testData))
+                .environment(\.colorScheme, .dark)
+            ContentView(store: RoomStore(rooms: testData))
+                .environment(\.layoutDirection, .rightToLeft)
+                .environment(\.locale, Locale(identifier: "ar"))
+        }
+        
     }
 }
 #endif
+
+struct RoomCell : View {
+    let room: Room
+    var body: some View {
+        return NavigationButton(destination: RoomDetail(room : room)) {
+            Image(room.thumbnailName)
+                .cornerRadius(8)
+            
+            VStack(alignment: .leading) {
+                Text(room.name)
+                Text("\(room.capacity) People")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+        }
+    }
+}
